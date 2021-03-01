bf9efe4311585627bd958907a78808e384bf5e60
Hasib787 <mr.hasib45@gmail.com>
2021-03-01 14:16:53 +0600

    added a cart file and add population

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import Country from './Component/Country';
+import Cart from './Component/Cart/Cart';
 
 function App() {
   const [countries, setCountries] = useState([]);
-
-  useEffect(()=>{
+  const [countryAdded, setCountryAdded] = useState([]);
+  useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then(data =>setCountries(data))
-    .catch(error => console.log(error))
+      .then(res => res.json())
+      .then(data => setCountries(data))
+      .catch(error => console.log(error))
   }, [])
 
-  const handleAddCountry=(country)=>{
-    console.log('Country Added',country);
+  const handleAddCountry = (country) => {
+    const newCountryAdded = [...countryAdded, country];
+    setCountryAdded(newCountryAdded);
   }
   return (
     <div className="App">
       <h1>Country Loaded: {countries.length}</h1>
-      <h3>Country Added: </h3>
+      <h3>Country Added: {countryAdded.length} </h3>
+      <Cart cart={countryAdded}></Cart>
       <ul>
         {
-          countries.map(country=> <Country country= {country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
+          countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
         }
       </ul>
-      
+
     </div>
   );
 }
